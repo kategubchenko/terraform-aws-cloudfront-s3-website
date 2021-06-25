5b1555f8189061c48477b9cb15d79f15235b75a7
Andrii Penchuk <penja7@gmail.com>
2021-06-25 12:31:44 +0300

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "cloudfront_s3_website_with_domain" {
-  source                 = "../"
-  hosted_zone            = "example.com"
-  domain_name            = "test.abc.example.com"
-  acm_certificate_domain = "*.abc.example.com"
-  use_default_domain     = false
-  upload_sample_file     = true
-  tags                   = var.tags
-}
-
 
 module "cloudfront_s3_website_without_domain" {
   source             = "../"
@@ -23,7 +13,7 @@ module "cloudfront_s3_website_without_domain" {
 
 variable "tags" {
   default = {
-    owner       = "Gangaraju"
+    owner       = "a.penchuk@scalr.com"
     application = "sample"
   }
 }
